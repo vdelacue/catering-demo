496c5e746a35460b6f6264745c8f91809bb114a6
Vanessa de La Cuetara <vdelacuetara@gmail.com>
2022-04-02 16:46:45 -0400

    Fixes text with icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
-/*!
-    * Start Bootstrap - Creative v6.0.3 (https://startbootstrap.com/themes/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-    */
-    (function($) {
+ (function($) {
   "use strict"; // Start of use strict
 
   // Smooth scrolling using jQuery easing
